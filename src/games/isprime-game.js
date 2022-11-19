f1e1e9dc45cea1895b1345740b53830e0e9fa895
SKornya <kornyakovS@gmail.com>
2022-11-19 18:34:51 +0300

    create brain-prime game

---
+++ src/games/isprime-game.js
@@ -0,0 +1,31 @@
+import game from '../index.js';
+
+const isPrimeNumber = (num) => {
+  if (num === 0 || num === 1) {
+    return false;
+  }
+  for (let i = 2; i < num / 2; i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const PrimeGame = () => {
+  const rules = 'Anser "yes" if given number is prime. Otherwise answer "no"';
+  const numbers = [];
+  const rigthAnswers = [];
+
+  for (let i = 0; i < 3; i += 1) {
+    const number = Math.round(Math.random() * 100);
+
+    numbers.push(number);
+    rigthAnswers.push(isPrimeNumber(number) ? 'yes' : 'no');
+  }
+
+  game(numbers, rigthAnswers, rules);
+};
+
+export default PrimeGame;
