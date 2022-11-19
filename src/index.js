ea54978fb7ba1c96176f1666ed1abef4746cf48a
SKornya <kornyakovS@gmail.com>
2022-11-19 13:31:52 +0300

    add index.js with common parts of games

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import readlineSync from 'readline-sync';
+
+const game = (gameQuestions, rigthAnswers, rules) => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  console.log(rules);
+
+  let rigthAnswersCount = 0;
+
+  for (let i = 0; i < 3; i += 1) {
+    const question = gameQuestions[i];
+    console.log(`Question: ${question}`);
+
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    if (rigthAnswers[i] === userAnswer) {
+      console.log('Correct!');
+      rigthAnswersCount += 1;
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${rigthAnswers[i]}'.`);
+      break;
+    }
+  }
+
+  if (rigthAnswersCount === 3) {
+    console.log(`Congratulations, ${name}!`);
+  }
+};
+
+export default game;
