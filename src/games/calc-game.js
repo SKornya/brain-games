6a57998e732603b939a3d585da3e463561139bba
SKornya <kornyakovS@gmail.com>
2022-11-19 13:49:34 +0300

    fixed calc-game

--- src/games/calc-game.js
+++ src/games/calc-game.js
@@ -9,27 +9,25 @@ const calcGame = () => {
   for (let i = 0; i < 3; i += 1) {
     const firstOperand = Math.round(Math.random() * 100);
     const secondOperand = Math.round(Math.random() * 100);
-    const operation = operations[Math.round(Math.random() * 3)];
+    const operation = operations[Math.floor(Math.random() * 3)];
 
     expressions.push(`${firstOperand} ${operation} ${secondOperand}`);
 
-    for (let j = 0; j < 3; j += 1) {
-      switch (operation) {
-        case '+':
-          rigthAnswers.push(firstOperand + secondOperand);
-          break;
-        case '-':
-          rigthAnswers.push(firstOperand - secondOperand);
-          break;
-        case '*':
-          rigthAnswers.push(firstOperand * secondOperand);
-          break;
-        default:
-          break;
-      }
+    switch (operation) {
+      case '+':
+        rigthAnswers.push(`${firstOperand + secondOperand}`);
+        break;
+      case '-':
+        rigthAnswers.push(`${firstOperand - secondOperand}`);
+        break;
+      case '*':
+        rigthAnswers.push(`${firstOperand * secondOperand}`);
+        break;
+      default:
+        break;
     }
   }
-  console.log(expressions, rigthAnswers);
+
   game(expressions, rigthAnswers, rules);
 };
 
