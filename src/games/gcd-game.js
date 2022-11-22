487d3a76d7cca836f28393a955811f741e0a3414
SKornya <kornyakovS@gmail.com>
2022-11-22 13:20:54 +0300

    fixed gcd-game

--- src/games/gcd-game.js
+++ src/games/gcd-game.js
@@ -11,6 +11,10 @@ const getGCD = (num1, num2) => {
     return Math.min(num1, num2);
   }
 
+  if (num1 === 0 || num2 === 0) {
+    return num1 === 0 ? num2 : num1;
+  }
+
   let result = 1;
 
   for (let div = 2; div <= Math.min(num1, num2) / 2; div += 1) {
