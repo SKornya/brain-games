a03f288a5a6250fcb968fcfc070c2cc7e5563a7e
SKornya <kornyakovS@gmail.com>
2022-11-22 12:30:06 +0300

    change executable function name

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 
-import calcGame from '../src/games/calc-game.js';
+import calcGameStart from '../src/games/calc-game.js';
 
-calcGame();
+calcGameStart();
--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 
-import gcdGame from '../src/games/gcd-game.js';
+import gcdGameStart from '../src/games/gcd-game.js';
 
-gcdGame();
+gcdGameStart();
